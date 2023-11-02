e3da0c79b9a2f5e8e9098eed4d67fe024b21c800
Rainer Regan (Exacode) <rainercoc@gmail.com>
2023-11-02 18:30:45 +0700

    Initial Commit

---
+++ Skinsense/SkinsenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkinsenseApp.swift
+//  Skinsense
+//
+//  Created by Rainer Regan on 02/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SkinsenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
