98faebe6652eb5cf71e83b31c37cc0df50bc930b
shelamines <109431534+shelamines@users.noreply.github.com>
2023-11-09 18:04:31 +0700

    tambah file

---
+++ Skinsense/Code/Presentation/Views/ScanResult/ScanResultView.swift
@@ -0,0 +1,18 @@
+//
+//  ScanResult.swift
+//  Skinsense
+//
+//  Created by Shelamines Julianni on 09/11/23.
+//
+
+import SwiftUI
+
+struct ScanResult: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScanResult()
+}
