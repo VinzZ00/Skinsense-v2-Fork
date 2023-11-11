7b43d508c7f7cb2278d12669e9af4120095a469f
Rainer Regan (Exacode) <rainercoc@gmail.com>
2023-11-11 12:16:39 +0700

    Update project files

--- Skinsense/Code/Presentation/Views/MainView/Tabs/ForYouTabView.swift
+++ Skinsense/Code/Presentation/Views/MainView/Tabs/ForYouTabView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import WrappingHStack
 
 struct ForYouTabView: View {
     @State var searchText: String = ""
@@ -22,36 +23,36 @@ struct ForYouTabView: View {
                         
                         // TODO: Integrate punya ines
                         WrappingHStack(alignment: .leading, horizontalSpacing: 10){
-                    PersonalizedProductIcon(text: "Cleanser", icon2: "FacialFoam")
-                    PersonalizedProductIcon(text: "Toner", icon2: "Toner")
-                    PersonalizedProductIcon(text: "Serum", icon2: "Serum")
-                    PersonalizedProductIcon(text: "Moisturizer", icon2: "Moisturizer")
-                }
-                WrappingHStack(alignment: .leading, horizontalSpacing: 10){
-                    PersonalizedProductIcon(text: "Exfoliator", icon2: "Exfo")
-                    PersonalizedProductIcon(text: "Sun Screen", icon2: "Sunscreen")
-                    PersonalizedProductIcon(text: "Face Mask", icon2: "faceMask")
-                    PersonalizedProductIcon(text: "Others", icon2: "Others")
-                }
-                Spacer()
-                    .frame(height: 25)
-                        
-                VStack(alignment: .leading){
-                    Text("Recently Scanned")
-                        .font(.title3)
-                        .bold()
-                    NavigationLink(destination: MainView()) {
-                        HStack {
-                            Text("Scanned Product #1")
-                                .foregroundStyle(.black)
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(.customDarkGrey)
+                            PersonalizedProductIcon(text: "Cleanser", icon2: "FacialFoam")
+                            PersonalizedProductIcon(text: "Toner", icon2: "Toner")
+                            PersonalizedProductIcon(text: "Serum", icon2: "Serum")
+                            PersonalizedProductIcon(text: "Moisturizer", icon2: "Moisturizer")
                         }
-                        .padding()
-                    }
-                    
-                }
+                        WrappingHStack(alignment: .leading, horizontalSpacing: 10){
+                            PersonalizedProductIcon(text: "Exfoliator", icon2: "Exfo")
+                            PersonalizedProductIcon(text: "Sun Screen", icon2: "Sunscreen")
+                            PersonalizedProductIcon(text: "Face Mask", icon2: "faceMask")
+                            PersonalizedProductIcon(text: "Others", icon2: "Others")
+                        }
+                        Spacer()
+                            .frame(height: 25)
+                        
+                        VStack(alignment: .leading){
+                            Text("Recently Scanned")
+                                .font(.title3)
+                                .bold()
+                            NavigationLink(destination: MainView()) {
+                                HStack {
+                                    Text("Scanned Product #1")
+                                        .foregroundStyle(.black)
+                                    Spacer()
+                                    Image(systemName: "chevron.right")
+                                        .foregroundColor(.customDarkGrey)
+                                }
+                                .padding()
+                            }
+                            
+                        }
                     }
                     
                     VStack(alignment: .leading) {
