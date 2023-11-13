1a8a444330df79a2ab990f6397b4c30354dff062
Rainer Regan (Exacode) <rainercoc@gmail.com>
2023-11-14 03:22:42 +0700

    Change to live API

--- Skinsense/Code/Utils/ConfigurationManager.swift
+++ Skinsense/Code/Utils/ConfigurationManager.swift
@@ -15,7 +15,7 @@ class ConfigurationManager {
     func apiBaseUrl() -> String {
         // Logic to read the base URL from a file or another source
 //        return "http://localhost:3005/"
-        return "https://e89c-103-136-59-166.ngrok-free.app/"
-//        return "https://api.skinsense.exacode.io/"
+//        return "https://e89c-103-136-59-166.ngrok-free.app/"
+        return "https://api.skinsense.exacode.io/"
     }
 }
